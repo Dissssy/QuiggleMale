c717ef9dd82ed5b6e56393dbef9e42551a0d6150
Dissssy <ethanconaway@yahoo.com>
2021-08-24 22:58:31 -0400

    every time

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord_components import DiscordComponents, Button, ButtonStyle, ActionRow
 import time
 from os.path import getsize
-testmode = True
+testmode = False
 training = False
 updaterrunning = False
 
